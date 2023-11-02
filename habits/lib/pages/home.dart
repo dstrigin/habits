134202b1fdc19d1b04df407c831417774e2eb974
Dima <dmitriystrigin04@gmail.com>
2023-11-02 22:03:09 +0300

    Изменил размер шапки

--- habits/lib/pages/home.dart
+++ habits/lib/pages/home.dart
@@ -18,6 +18,7 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.cyan,
         elevation: 0.0,
         centerTitle: true,
+        toolbarHeight: 65,
       ),
     );
   }
