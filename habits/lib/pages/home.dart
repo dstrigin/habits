4d16e59982e93cf7abda5562ed9c9a1f4f2daaa5
Dima <dmitriystrigin04@gmail.com>
2023-11-14 12:08:47 +0300

    Список привычек пользователя
    Добавил иконки и заглушку для отображения привычек пользователя (пока что заглушки)

--- habits/lib/pages/home.dart
+++ habits/lib/pages/home.dart
@@ -12,16 +12,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: 
-          Row( 
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            SvgPicture.asset('assets/icons/hp_bar.svg')
-          ],)
-      ),
+        appBar: appBar(),
+        body: ListView(
+          padding: const EdgeInsets.only(top: 5, bottom: 5),
+          scrollDirection: Axis.vertical,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 5, bottom: 10),
+              child: SvgPicture.asset(
+                  'assets/icons/hp_bar.svg',
+                  alignment: Alignment.center
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/cigarette.svg',
+                  width: 40,
+                  height: 40,
+                  alignment: Alignment.centerLeft,
+                ),
+                const Text(
+                  'Курение',
+                  locale: Locale('ru'),
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                ),
+                IconButton(
+                    onPressed: null,
+                    icon: SvgPicture.asset(
+                      'assets/icons/info.svg',
+                      alignment: Alignment.centerRight,
+                      width: 50,
+                      height: 50
+                    ),
+                )
+
+              ],
+            ),
+            Container(
+                width: 10,
+                height: 50,
+                padding: const EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.black)
+                ),
+                child: const Text(
+                  'Habit #1',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30
+                  ),
+                )
+            )
+          ],
+        )
     );
   }
 }
