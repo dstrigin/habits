6d413952cd97766f22649404ec8f182626425013
dedicklo <145215000+dedicklo@users.noreply.github.com>
2023-11-26 14:58:42 +0300

    Отредактировал документацию
    Добавлена компоновка и разделение по структурным блокам

--- habits/lib/pages/documentation.dart
+++ habits/lib/pages/documentation.dart
@@ -2,37 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:habits/elements/appBars.dart';
 
 class DocumentationPage extends StatelessWidget {
-  const DocumentationPage ({super.key});
+  const DocumentationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: docAppBar(),
-      body: ListView(
-        padding: const EdgeInsets.only(left: 5, top:10, bottom: 12),
-        children: [
-          RichText(
-            text: const TextSpan(
-            style: TextStyle(fontSize: 24, color: Colors.black, fontFamily: 'Bahnschrift'),
-            children: <TextSpan> [
-              TextSpan(text: '    Добро пожаловать в трекер привычек ',),
-              TextSpan(text: '"habits."', style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: '!\n\n    У нас есть одна цель - обратить внимание пользователей на свое здоровье через взгляд на свои привычки, и мы подбираемся к ней посредством разработки данного приложения.\n\n    На ',),
-              TextSpan(text: 'домашнем экране', style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: ' будут отображаться все привычки, которые у вас есть на данный момент.\n\n    Чтобы добавить привычку нажмите на '),
-              TextSpan(text: '"плюс"', style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: ' - вы увидите список распространенных привычек, представленный нами, и сможете выбрать то, что подходит вам (а в поздних версиях - создать свою привычку). Для этого нажмите на ее название и кнопку "добавить" во всплывшем окне.\n\n'),
-              TextSpan(text: '    Важно!', style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: ' Не стоит закрывать глаза на свои плохие привычки. Особенностью нашего приложения является наличие '),
-              TextSpan(text: '"полосы здоровья"', style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: ' - индикатора, который говорит вам о вашем состоянии в зависимости от количества и качества привычек.\n\n    Об истории добавления привычек и ежедневных изменениях полосы здоровья вы сможете посмотреть на второй странице приложения - '),
-              TextSpan(text: '"история"', style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: '.\n\n    Если у вас есть вопросы или пожелания по поводу работы приложения - я с радостью готов с вами контактировать!\n\n    tg: @dstrigin  '),
-              ]
-            )
-          ),
-        ]
-      )
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Добро пожаловать в трекер привычек "habits"',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.cyan,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'У нас есть одна цель - обратить внимание пользователей на свое здоровье через взгляд на свои привычки, и мы подбираемся к ней посредством разработки данного приложения.',
+              style: TextStyle(fontSize: 18, color: Colors.black87),
+            ),
+            SizedBox(height: 16),
+            Divider(color: Colors.cyan),
+            Text(
+              'На домашнем экране будут отображаться все привычки, которые у вас есть на данный момент.',
+              style: TextStyle(fontSize: 18, color: Colors.black87),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Чтобы добавить привычку, нажмите на "плюс" - вы увидите список распространенных привычек, представленный нами, и сможете выбрать то, что подходит вам. Для этого нажмите на ее название и кнопку "добавить" во всплывшем окне.',
+              style: TextStyle(fontSize: 18, color: Colors.black54),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Важно! Не стоит закрывать глаза на свои плохие привычки. Особенностью нашего приложения является наличие "полосы здоровья" - индикатора, который говорит вам о вашем состоянии в зависимости от количества и качества привычек.',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Об истории добавления привычек и ежедневных изменениях полосы здоровья вы сможете посмотреть на второй странице приложения - "история".',
+              style: TextStyle(fontSize: 18, color: Colors.black54),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Если у вас есть вопросы или пожелания по поводу работы приложения - я с радостью готов с вами контактировать!',
+              style: TextStyle(fontSize: 18, color: Colors.black54),
+            ),
+            Divider(color: Colors.cyan),
+            SizedBox(height: 8),
+            Text(
+              'tg: @dstrigin',
+              style: TextStyle(fontSize: 18, color: Colors.cyan, decoration: TextDecoration.underline),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
