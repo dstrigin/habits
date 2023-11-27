9d8d2ef2fbeb67b56ef62fac13bedd54005d0ad8
Dima <dmitriystrigin04@gmail.com>
2023-11-27 13:26:34 +0300

    Подправил дизайн документации

--- habits/lib/pages/documentation.dart
+++ habits/lib/pages/documentation.dart
@@ -16,7 +16,7 @@ class DocumentationPage extends StatelessWidget {
             Text(
               'Добро пожаловать в трекер привычек "habits"',
               style: TextStyle(
-                fontSize: 24,
+                fontSize: 28,
                 fontWeight: FontWeight.bold,
                 color: Colors.cyan,
               ),
@@ -24,39 +24,42 @@ class DocumentationPage extends StatelessWidget {
             SizedBox(height: 16),
             Text(
               'У нас есть одна цель - обратить внимание пользователей на свое здоровье через взгляд на свои привычки, и мы подбираемся к ней посредством разработки данного приложения.',
-              style: TextStyle(fontSize: 18, color: Colors.black87),
+              style: TextStyle(fontSize: 22, color: Colors.black87),
             ),
             SizedBox(height: 16),
             Divider(color: Colors.cyan),
             Text(
               'На домашнем экране будут отображаться все привычки, которые у вас есть на данный момент.',
-              style: TextStyle(fontSize: 18, color: Colors.black87),
+              style: TextStyle(fontSize: 22, color: Colors.black87),
             ),
             SizedBox(height: 16),
             Text(
               'Чтобы добавить привычку, нажмите на "плюс" - вы увидите список распространенных привычек, представленный нами, и сможете выбрать то, что подходит вам. Для этого нажмите на ее название и кнопку "добавить" во всплывшем окне.',
-              style: TextStyle(fontSize: 18, color: Colors.black54),
+              style: TextStyle(fontSize: 22, color: Colors.black54),
             ),
             SizedBox(height: 16),
             Text(
               'Важно! Не стоит закрывать глаза на свои плохие привычки. Особенностью нашего приложения является наличие "полосы здоровья" - индикатора, который говорит вам о вашем состоянии в зависимости от количества и качества привычек.',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black87),
             ),
             SizedBox(height: 16),
             Text(
               'Об истории добавления привычек и ежедневных изменениях полосы здоровья вы сможете посмотреть на второй странице приложения - "история".',
-              style: TextStyle(fontSize: 18, color: Colors.black54),
+              style: TextStyle(fontSize: 22, color: Colors.black54),
             ),
             SizedBox(height: 16),
             Text(
               'Если у вас есть вопросы или пожелания по поводу работы приложения - я с радостью готов с вами контактировать!',
-              style: TextStyle(fontSize: 18, color: Colors.black54),
+              style: TextStyle(fontSize: 22, color: Colors.black54),
             ),
             Divider(color: Colors.cyan),
             SizedBox(height: 8),
             Text(
               'tg: @dstrigin',
-              style: TextStyle(fontSize: 18, color: Colors.cyan, decoration: TextDecoration.underline),
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.cyan,
+                decoration: TextDecoration.underline),
             ),
           ],
         ),
